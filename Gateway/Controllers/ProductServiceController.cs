011f6b4d46ecece6f395b2d2bc4519175cca7fb7
MICHALLL <micher.pl@gmail.com>
2017-05-29 00:04:01 +0200

    Connect with ProductService

---
+++ Gateway/Controllers/ProductServiceController.cs
@@ -0,0 +1,29 @@
+﻿using Gateway.Models;
+using Gateway.ProductServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Gateway.Controllers
+{
+    public class ProductServiceController : ApiController
+    {
+        ProductServiceClient client = new ProductServiceClient();
+
+        
+        [Route("api/getAllProducts")]
+        public IEnumerable<ProductServiceReference.Product> GetAllProducts()
+        {
+            return client.GetAllProducts();
+        }
+
+        [Route("api/DecreaseProductCount/{id}/{count}")]
+        public bool DecreaseProductCount(int id, int count)
+        {
+            return client.DecreaseProductCount(id, count);
+        }
+    }
+}
---
+++ Gateway/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gateway.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string postCode { get; set; }
+    }
+}
